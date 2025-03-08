a5826a7cda1f67d890fcdfe89b5026e737543d65
cherfa_e <cherfaouimehdi@yahoo.com>
2025-03-08 21:49:26 +0100

    variables

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
 #this is a python file
 print("Hello world !!!")
+
+#variables
+
+#string
+first_name="first-name"
+last_name='last-name'
+print(first_name)
+print(last_name)
+#to print text with a variable
+print(f"Hello {first_name}")
+
+#integers
+age=25
+print(f"you are {age} years old")
+
+#Float
+price=10.99
+print(f'the price is {price} euros')
+
+#boolean
+is_student=True
+is_employee=False
+print(f'you are a student :{is_student}')
+print(f'you are an employee :{is_employee}')
+
+#if_else
+if is_student:
+     print("he is a student")
+else:
+     print("he is employee")    
